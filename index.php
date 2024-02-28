68047b537d2a1030dc1d0b28b79e4818d683ea4b
mahbub <mahbub.rahman@s2019.tu-chemnitz.de>
2024-02-29 00:02:17 +0100

    Initialize Project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>PHP - Docker</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+   
+
+	<h1>PHP Docker Practice</h1>
+	<img src="public/images/docker.png">
+
+</body>
+
+</html>
